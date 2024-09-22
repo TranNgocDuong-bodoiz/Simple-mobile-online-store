9ae6c3dcbb7ab89981ebb6af66316c7bc2a9d2b8
TranNgocDuong-bodoiz <22111061237@hunre.edu.vn>
2024-09-22 12:42:55 +0700

    add index file and folder

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>simple </title>
+    <link href="fontawesome-free-6.6.0-web/css/all.min.css" rel="stylesheet" >
+    <link href="CSS/header.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="CSS/main.css">
+    <link rel="stylesheet" href="CSS/container.css">
+
+</head>
+<body>
+<div class="container">
+    <!-- header div -->
+    <div class="page_header">
+        <!-- 1.logo / 2.danh_muc/ 3.search_bar / 4.hotline/ 5.tra_cuu_don_hang/ 6.gio_hang/ 7.tai_khoan -->
+        <div class="logo">
+            <div class="shop_name">
+                <span>SIMPLE MOBILE</span>
+            </div>
+            <div class="shop_content">
+                favourite online store
+            </div>
+        </div>
+        <div class="danh_muc_container">
+            <div class="danh_muc">
+                <i class="fa-solid fa-bars"></i>
+                <span>Danh mục</span>
+            </div>
+            <ul class="danh_muc_list">
+                <li class="gap_item"><i class="fa-solid fa-angle-down"></i></li>
+                <li class="danh_muc_item">
+                    <i class="fa-solid fa-mobile-screen-button"></i>
+                    <a href="#">Điện thoại</a>
+                </li>
+                <li class="danh_muc_item">
+                    <i class="fa-regular fa-tablet"></i>
+                    <a href="#">Tablet</a>
+                </li>
+                <li class="danh_muc_item">
+                    <i class="fa-solid fa-headphones"></i>
+                    <a href="#">Phụ kiện</a>
+                </li>
+                <li class="danh_muc_item">
+                    <i class="fa-solid fa-recycle"></i>
+                    <a href="#">Hàng cũ</a>
+                </li>
+                <li class="danh_muc_item">
+                    <i class="fa-regular fa-newspaper"></i>
+                    <a href="#">Tin tức</a>
+                </li>
+            </ul>
+        </div>
+        <div class="search_bar">
+            <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+            <input type="text" placeholder="Hôm nay bạn cần tìm gì" autocomplete="off">
+        </div>
+        <div class="hotline">
+            <a href="#">
+                <div class="hotline_icon"><i class="fa-solid fa-phone-volume"></i></div>
+                <div class="hotline_infor">
+                    <span id="hotline_text">gọi mua ngay</span>
+                    <span id="hotline_number">0888888889</span>
+                </div>
+            </a>
+        </div>
+        <div class="don_hang">
+            <a href="#">
+                <div class="don_hang_icon"><i class="fa-solid fa-truck"></i></div>
+                <div class="don_hang_infor"><span id="don_hang_text">tra cứu đơn hàng</span></div>
+            </a>
+        </div>
+        <div class="gio_hang">
+            <a href="#">
+                <div class="gio_hang_icon"><i class="fa-solid fa-cart-shopping"></i></div>
+                <div class="gio_hang_infor"><span id="gio_hang_text">giỏ hàng</span></div>
+            </a>
+        </div>
+        <div class="tai_khoan">
+            <div class="tai_khoan_icon">
+                <i class="fa-regular fa-circle-user"></i>
+            </div>
+            <div class="tai_khoan_text">
+                <span>tài khoản</span>
+            </div>
+        </div>
+    </div> <!-- Đóng thẻ page_header -->
+    <div class="main">
+        <div class="banner">
+            <div class="big_banner">
+                <img src="imgaes/laptop-ai-banner-chung-slide.webp" alt="" width="100%" height="100%">
+                <span style="text-align: center ; color:gray;">uy tín tạo nên thương hiệu</span>
+            </div>
+            <div class="sub_banner">
+                <div class="sub_banner1"><img src="imgaes/b2s-2024-right-banner-laptop.webp" alt="" width="100%"> </div>
+                <div class="sub_banner2"><img src="imgaes/m55-gia-moi-right-banner-30-8-2024.webp" alt="" width="100%"></div>
+                <div class="sub_banner3"><img src="imgaes/right-banner-dat-truoc-iphone-16.webp" alt="" width="100%"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- Đóng thẻ container -->
+
+</body>
+</html>
